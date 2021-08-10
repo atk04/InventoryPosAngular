cc8e07504958198e26b8e59317c220d54073e22d
atk04 <49226136+atk04@users.noreply.github.com>
2021-08-10 20:47:53 +0630

    create category service

---
+++ src/app/services/product-category.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductCategory } from '../common/product-category';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductCategoryService {
+private categoryUrl='http://localhost:8080/api/product-category';
+  constructor(private httpClient:HttpClient) { }
+  getProductCategories():Observable<ProductCategory[]>{
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response=>response._embedded.productCategory)
+    )
+  }
+  getProductCategoriesPaginate(thePage:number,thePageSize:number):Observable<GetResponseProductCategory>{
+    const searchUrl=`${this.categoryUrl}?page=${thePage}&size=${thePageSize}`;
+    return this.httpClient.get<GetResponseProductCategory>(searchUrl);
+  }
+}
+
+
+interface GetResponseProductCategory{
+  "_embedded": {
+    "productCategory": ProductCategory[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
+  }
+}
