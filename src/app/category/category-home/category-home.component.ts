d79467511c1ee79e8aaaff2d9604e22db8973752
atk04 <49226136+atk04@users.noreply.github.com>
2021-08-10 21:03:50 +0630

    Implement CRUD with angular material for category on category module and component

--- src/app/category/category-home/category-home.component.ts
+++ src/app/category/category-home/category-home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductCategoryService } from 'src/app/services/product-category.service';
+import { InventoryPos } from 'src/app/validators/inventory-pos';
 @Component({
   selector: 'app-category-home',
   templateUrl: './category-home.component.html',
@@ -7,9 +11,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryHomeComponent implements OnInit {
 
-  constructor() { }
+
+  // MatPaginator Output
+  pageEvent: PageEvent;
+
+  // MatPaginator Inputs
+  thePageNumber: number = 1;
+  length:number = 0;
+  pageSize:number = 3;
+  pageSizeOptions: number[] = [3, 6, 9, 12];
+
+  onPaginate(pageEvent:PageEvent){
+    this.pageSize=+pageEvent.pageSize;
+    this.thePageNumber=+pageEvent.pageIndex+1;
+    this.listProductCategories();
+
+  }
+
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
+
+
+
+productCategories:ProductCategory[];
+  constructor(private formBuilder:FormBuilder,private productCategoryService:ProductCategoryService) { }
+
+  categoryFormGroup:FormGroup;
 
   ngOnInit(): void {
+
+    this.categoryFormGroup=this.formBuilder.group(
+      {
+        categoryName: new FormControl('',[Validators.required,Validators.minLength(2),InventoryPos.notOnlyWhitespace])
+      }
+    )
+
+
+    this.listProductCategories();
+  }
+
+
+  get categoryName(){return this.categoryFormGroup.get('categoryName');}
+  listProductCategories() {
+    this.productCategoryService.getProductCategoriesPaginate(this.thePageNumber-1,this.pageSize).subscribe(
+
+      this.processResult()
+    )
+  }
+
+  processResult() {
+    return (data) => {
+      this.productCategories = data._embedded.productCategory;
+      this.thePageNumber = data.page.number ;
+      this.pageSize = data.page.size;
+      this.length = data.page.totalElements;
+    };
+  }
+
+  onSubmit(){
+
   }
 
 }
--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,15 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryHomeComponent } from './category-home/category-home.component';
+import { SharedModule } from '../theme/shared/shared.module';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+
+
+const materialModules = [
+  MatButtonModule,
+  MatIconModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
+];
 
 
 @NgModule({
   declarations: [CategoryHomeComponent],
   imports: [
     CommonModule,
-    CategoryRoutingModule
-  ]
+    CategoryRoutingModule,
+    SharedModule,
+    materialModules
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CategoryModule { }
