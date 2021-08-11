f87716e1c9ccd042f281336828ed14b5a7e9a93c
atk04 <49226136+atk04@users.noreply.github.com>
2021-08-11 11:15:55 +0630

    background style changed using app-config.ts

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -4,7 +4,7 @@ export class NextConfig {
     subLayout: '', // horizontal-2
     collapseMenu: false,
     layoutType: 'menu-light', // menu-dark, menu-light, dark
-    headerBackColor: 'background-blue', // background-blue, background-red, background-purple, background-info, background-dark
+    headerBackColor: 'background-info', // background-blue, background-red, background-purple, background-info, background-dark
     rtlLayout: false,
     navFixedLayout: false,
     headerFixedLayout: false,
