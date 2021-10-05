5067bc2d0d0b2b875a2f314c5b12db3cc12fcb85
Aung Thein Kyaw <aungtheinkyaw.atk04@gmail.com>
2021-10-05 14:31:06 +0630

    fix unauthorize access to admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,22 +23,22 @@ const routes: Routes = [
         loadChildren: () => import('./demo/pages/sample-page/sample-page.module').then(module => module.SamplePageModule)
       },
       {
-        path:'company-page',loadChildren:()=>import('./company/company.module').then(module=>module.CompanyModule)
+        path:'company-page',canActivate:[AuthGuardService],loadChildren:()=>import('./company/company.module').then(module=>module.CompanyModule)
       },
       {
-        path:'category-page',loadChildren:()=>import('./category/category.module').then(module=>module.CategoryModule)
+        path:'category-page',canActivate:[AuthGuardService],loadChildren:()=>import('./category/category.module').then(module=>module.CategoryModule)
       },
       {
-        path:'add-product-page',loadChildren:()=>import('./product/add-product/add-product.module').then(module=>module.AddProductModule)
+        path:'add-product-page',canActivate:[AuthGuardService],loadChildren:()=>import('./product/add-product/add-product.module').then(module=>module.AddProductModule)
       },
       {
-        path:'product-list-page',loadChildren:()=>import('./product/product-list/product-list.module').then(module=>module.ProductListModule)
+        path:'product-list-page',canActivate:[AuthGuardService],loadChildren:()=>import('./product/product-list/product-list.module').then(module=>module.ProductListModule)
       },
       {
-        path:'create-order-page',loadChildren:()=>import('./order/create-order/create-order.module').then(module=>module.CreateOrderModule)
+        path:'create-order-page',canActivate:[AuthGuardService],loadChildren:()=>import('./order/create-order/create-order.module').then(module=>module.CreateOrderModule)
       },
       {
-        path:'order-list-page',loadChildren:()=>import('./order/order-list/order-list.module').then(module=>module.OrderListModule)
+        path:'order-list-page',canActivate:[AuthGuardService],loadChildren:()=>import('./order/order-list/order-list.module').then(module=>module.OrderListModule)
       }
     ]
   },
