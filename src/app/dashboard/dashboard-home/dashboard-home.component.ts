c204b13a08073bb71215a8e7c138d097f5d0f01f
Aung Thein Kyaw <aungtheinkyaw.atk04@gmail.com>
2021-10-05 17:48:45 +0630

    Logout implementation finish

--- src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,26 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {OAuthService, AuthConfig } from 'angular-oauth2-oidc';
-export const authConfig: AuthConfig = {
-  issuer:'https://dev-96372766.okta.com/oauth2/default',
-  redirectUri:window.location.origin,
-  clientId:'0oa2400capP8XEsIJ5d7',
-  scope: 'openid profile email'
-}
+
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: ['./dashboard-home.component.scss']
 })
 export class DashboardHomeComponent implements OnInit {
-  constructor(private oauthService:OAuthService) {
-    this.oauthService.configure(authConfig);
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
-  }
-
-  logout(){
-    this.oauthService.logOut();
+  constructor() {
 
   }
+
 
   ngOnInit() {
   }
--- src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
+++ src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
@@ -1,15 +1,26 @@
-import {Component, OnInit} from '@angular/core';
-import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
-
+import { Component, OnInit } from '@angular/core';
+import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
+import { OAuthService, AuthConfig } from 'angular-oauth2-oidc';
+export const authConfig: AuthConfig = {
+  issuer: 'https://dev-96372766.okta.com/oauth2/default',
+  redirectUri: window.location.origin,
+  clientId: '0oa2400capP8XEsIJ5d7',
+  scope: 'openid profile email',
+};
 @Component({
   selector: 'app-nav-right',
   templateUrl: './nav-right.component.html',
   styleUrls: ['./nav-right.component.scss'],
-  providers: [NgbDropdownConfig]
+  providers: [NgbDropdownConfig],
 })
 export class NavRightComponent implements OnInit {
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
 
-  constructor() { }
-
-  ngOnInit() { }
+  logout() {
+    this.oauthService.logOut();
+  }
+  ngOnInit() {}
 }
